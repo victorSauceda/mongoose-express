e01b0c0895e8954404675df99ef17ee54c1ab6e7
Onkur Lal <onkur24lal@gmail.com>
2022-11-16 08:13:17 -0500

    day 8 HW

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const router = require('./router');
 const welcome = require('./module-practice.js')
+const bookData = require('./book-data')
 
 app.use('/', router);
-console.log(welcome('Onkur'));
+// console.log(welcome('Onkur'));
+
+app.get('/:id', (req, resp) => {
+    resp.render('index', {title:bookData.books[req.params.id]['title'], author:bookData.books[req.params.id]['author']})
+})
+
+app.set('views', './views');
+app.set('view engine', 'pug');
 app.listen(3001)
--- router.js
+++ router.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const bookData = require('./book-data')
 
-router.get('/games/:id', (req, resp)=> {
-    resp.send(`Get Route on router ${req.params.id}`)
-})
+// router.get('/games/:id', (req, resp)=> {
+//     resp.send(`Get Route on router ${req.params.id}`)
+// })
+// router.get('/', (req, resp) => {
+//     resp.send('Get Route for home page')
+// })
+// router.post('/:games/id', (req, resp) => {
+//     resp.send(`3rd route ${req.params.games}`)
+// })
+
 router.get('/', (req, resp) => {
-    resp.send('Get Route for home page')
+    resp.send(bookData.books)
 })
-router.post('/:games/id', (req, resp) => {
-    resp.send(`3rd route ${req.params.games}`)
+
+router.delete('/:id', (req, resp) => {
+    resp.send(`Book ${req.params.id} has been successfully deleted!`);
 })
 
 module.exports = router
