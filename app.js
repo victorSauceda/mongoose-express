5ffc6be38e9fc4dd47a5220a7cc673ee8fc89286
victorSauceda <victorasauceda@gmail.com>
2022-11-28 21:49:19 -0800

    changes

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const router = require('./routes/index-route')
-const welcome = require('./module-practice.js')
-const bookData = require('./book-data')
-app.set('views', './views');
-app.set('view engine', 'pug');
+const router = require("./routes/index-route");
+const welcome = require("./module-practice.js");
+const bookData = require("./book-data");
+const favicon = require("serve-favicon");
+const path = require("path");
+app.set("views", "./views");
+app.set("view engine", "pug");
 app.use(express.json());
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+app.use("/", router);
 
-app.use('/', router);
-console.log(welcome('Onkur'));
-
-
-
-app.listen(3001)
+app.listen(3000);
